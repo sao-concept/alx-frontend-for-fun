4cf9a0a09e93cedba18bf5cab0c61d42066be511
Azeez Saka <saoconcept1@gmail.com>
2024-04-22 10:54:45 -0700

    Modified markdown2html.py script

--- markdown2html.py
+++ markdown2html.py
@@ -31,6 +31,7 @@ def convert_markdown_to_html(input_file: str, output_file: str) -> None:
         sys.stderr.write(f"Missing {input_file}\n")
         sys.exit(1)
 
+
 def main() -> None:
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Convert Markdown file to HTML file")
@@ -40,6 +41,7 @@ def main() -> None:
 
     # Convert Markdown to HTML
     convert_markdown_to_html(args.input_file, args.output_file)
+
 
 if __name__ == "__main__":
     main()
